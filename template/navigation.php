e07ede592bd77b37f8bb671469f05156f220adce
AmirStha <amirshresthapj@gmail.com>
2016-06-01 19:31:22 +0545

    arrange pjnav

--- template/navigation.php
+++ template/navigation.php
@@ -14,8 +14,8 @@
                     <li<?php if($pageid == 1){echo ' class="active"';} ?>><a href="?page=1">Home</a></li>
                     <li<?php if($pageid == 2){echo ' class="active"';} ?>><a href="?page=2">About Us</a></li>
 -->
-                    <li><a href="#">FAQ</a></li>
                     <li><a href="#">Contact</a></li>
+                    <li><a href="#">FAQ</a></li>
                 </ul>
             </div>    
         </nav><!-- END nav Main Navigation-->
