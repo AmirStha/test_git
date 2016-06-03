d37e1fda8e4d7abf158d91c0d81f38662a77347e
AmirStha <amirshresthapj@gmail.com>
2016-06-03 15:38:51 +0545

    Changed the nav contact to contact Sublime

--- template/navigation.php
+++ template/navigation.php
@@ -14,7 +14,7 @@
                     <li<?php if($pageid == 1){echo ' class="active"';} ?>><a href="?page=1">Home</a></li>
                     <li<?php if($pageid == 2){echo ' class="active"';} ?>><a href="?page=2">About Us</a></li>
 -->
-                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Contact Sublime</a></li>
                     <li><a href="#">FAQ</a></li>
                 </ul>
             </div>    
