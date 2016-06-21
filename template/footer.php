9b7607283980c33d94305ce8f4bfd577ce420086
AmirStha <amirshresthapj@gmail.com>
2016-06-21 11:16:22 +0545

    Changes the footer contents

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
         <footer id="footer">
             <div class="container">
                 <p>
-                    This is a footer
+                    This is a footer. This will be modified later
                 </p>
             </div>
         </footer><!--End Footer-->
